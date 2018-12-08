83a402512b00a7e89f742aa738a21e7e0276fc15
Chaz <c.bruscato97@gmail.com>
2018-12-07 20:33:49 -0600

    Added actual conversion functionality.

--- ConversionUI/ConversionUI/ConversionViewController.swift
+++ ConversionUI/ConversionUI/ConversionViewController.swift
@@ -56,9 +56,80 @@ class ConversionViewController: UIViewController {
         self.present(alert, animated:true, completion: nil)
     }
     
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        inputDisplay.text?.append(sender.currentTitle!)
+        convert()
+    }
+    
+    @IBAction func clearPressed(_ sender: UIButton) {
+        inputDisplay.text = ""
+        outputDisplay.text = ""
+    }
+    
+    @IBAction func decimalPressed(_ sender: UIButton) {
+        if let inputString = inputDisplay.text{
+            if !inputString.contains("."){
+                //only append a decimal if there is not already one in the number.
+                inputDisplay.text?.append(".")
+                convert()
+            }
+        }
+    }
+    
+    @IBAction func changePosNeg(_ sender: Any) {
+        if let currentInput = inputDisplay.text{
+            if var currentInputDouble = Double(currentInput){
+                currentInputDouble = currentInputDouble * -1
+                inputDisplay.text = String(currentInputDouble)
+                convert()
+            }else{
+                //if there is nothing currently in the input text field, start it with a "-"
+                inputDisplay.text = "-"
+            }
+        }
+    }
+    
+    
     func ChangeInputOutput(input: String, output: String){
-        inputDisplay.text = input
-        outputDisplay.text = output
+        inputDisplay.placeholder = input
+        outputDisplay.placeholder = output
+    }
+    
+    func celsiusToFahrenheit(_ celsius: Double) -> Double{
+        return celsius * (9/5) + 32
+    }
+    
+    func fahrenheitToCelsius(_ fahrenheit: Double) -> Double{
+        return (fahrenheit - 32) * (5/9)
+    }
+    
+    func milesToKilometers(_ miles : Double) -> Double{
+        return miles * 1.61
+    }
+    
+    func kilometersToMiles(_ kilometers : Double) -> Double{
+        return kilometers * 0.62;
+    }
+    
+    func convert(){
+        if let inputText = inputDisplay.text,
+            let input = Double(inputText){
+            var output : Double
+            switch currentConversion.inputUnit{
+            case "°F":
+                output = fahrenheitToCelsius(input)
+            case "°C":
+                output = celsiusToFahrenheit(input)
+            case "mi":
+                output = milesToKilometers(input)
+            case "km":
+                output = kilometersToMiles(input)
+            default:
+                return
+            }
+            outputDisplay.text = String(format: "%.3f" , output) + " " + currentConversion.outputUnit
+        }
     }
 
 }
